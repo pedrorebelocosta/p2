841d1a288a4e78d7897828ada5bf3c718538a911
pedrocosta <pedrorebelocosta@gmail.com>
2024-05-08 11:23:37 +0100

    add missing module files

---
+++ web-api/src/main/java/org/nolhtaced/webapi/ServletInitializer.java
@@ -0,0 +1,11 @@
+package org.nolhtaced.webapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebapiApplication.class);
+	}
+}
